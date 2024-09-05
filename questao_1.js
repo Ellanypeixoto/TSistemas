876ca55b41b239dbd544d821ea77f2a561cf85b2
Ellany <ellany@msn.com>
2024-09-05 10:04:53 -0300

    Questões da avaliação
    Questões da avaliação.

---
+++ questao_1.js
@@ -0,0 +1,14 @@
+//Inicialmente, declaramos as variáveis:
+
+var indice = 13
+var k = 0
+soma = 0
+
+// Em seguida, vamos declarar o comando while, que vai executar uma rotina contínua verdadeira, 
+//até que a condição seja falsa e o programa encerre:
+
+while (k<indice) {
+    console.log(soma)
+    k=k+1; soma = soma+k
+
+}
